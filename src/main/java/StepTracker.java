c790485d42d6ad5d0c73a1d01fe971c85e8d8629
Bosco-Wong <142552491+Bosco-Wong@users.noreply.github.com>
2024-04-22 20:36:40 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,26 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
-} 
+private int max;
+ private int days;
+ private ArrayList <Integer> data;
+ StepTracker(int x ) {
+  days = 0; 
+  data= new <Integer>() ArrayList; 
+  max = a;
+ }
+ public void addDailySteps(int b){
+  data.add(b);
+ }
+ public int activeDays(){ 
+  for(int x = 0; x < data.size();x++)
+   if(data.get(x) >= a)
+    count++; 
+ }
+ public int averageSteps(){
+  int y = 0;
+  for(int x  = 0; x < data.size(); x++)
+   y += data.get(x);
+  return y/data.size();
+}
+}
